f878e644a62b4b04e4009d12cf51bfa805bad519
Adenhall <jamesadenhall@gmail.com>
2020-05-14 04:04:05 +0700

    Minor updates

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-let profile = document.getElementById("gameProfile")
-let signBtn = document.getElementById("signBtn")
+let profile = document.getElementById("gameProfile").value
 
 function loginCred() {
-  document.getElementById("login").innerHTML = `Welcome aboard, master ${profile.value}`;
+  document.getElementById("login").innerHTML = `Welcome aboard, master ${profile}`;
+  console.log(profile)
 }
-signBtn.addEventListener('click',loginCred)
 
 let rand = Math.floor(Math.random() * 100) + 1;
 let answer = document.getElementById("gameAnswer")
@@ -17,23 +16,29 @@ let playGameNow = document.getElementById('playGame')
 
 function playGame() {
   let x=Math.floor(Math.random()*10)
-  i++
-  document.getElementById('counts').innerHTML=`You've tried ${i} time(s)`
-  if (rand > answer.value) {
-    document.getElementById('gameStatus2').innerHTML = `Too lowwwww`
-    document.getElementById('gameStatus').innerHTML = `${a[x]}`
+
+  if (answer.value <= 0) {
+    document.getElementById('gameStatus2').innerHTML = "Don't be silly. Enter from 1 to 100 please"
+    document.getElementById('gameStatus').innerHTML = null
+    i=i-1
   }
   else if (rand < answer.value) {
     document.getElementById('gameStatus2').innerHTML = `Too highhh`
+    document.getElementById('gameStatus').innerHTML = `${a[x]}`
+  }
+  else if (rand > answer.value) {
+    document.getElementById('gameStatus2').innerHTML = `Too lowwww`
     document.getElementById('gameStatus').innerHTML = `${a[x]}`
   }
   else if (rand == answer.value) {
     document.getElementById('gameStatus').innerHTML = `Wow`
     document.getElementById('gameStatus2').innerHTML = `Correct`
   }
+  i++
+  document.getElementById('counts').innerHTML=`You've tried ${i} time(s)`
 }
+  console.log(i)
   let history = []
   history.push(answer)
-  console.log(history)
   playGameNow.addEventListener('click',playGame)
   document.getElementById('gameHistory').innerHTML=`History: ${history}`
